6ee96e63f7cdb568078791e16e60aadb4fa5fc16
bhomik005 <bhomik005@gmail.com>
2024-09-16 19:41:12 +1000

    added lexer.go and updated the test file.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,2 +1,44 @@
-// lexer/lexer_test.go
+// lexer takes the source code as input and output the tokens
 package lexer
+
+// importing packages
+import (
+	"monkey/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	type TestTokenType struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}
+
+	// tests is a slice of struct - allows you to store multiple instances of struct in single variable
+	tests := []TestTokenType{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	// creates a new lexer instance with the input
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokenType wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
